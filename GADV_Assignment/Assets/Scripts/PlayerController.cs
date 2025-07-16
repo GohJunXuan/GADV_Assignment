3d0fe4a80297f0896b1508ca17c44ba89aa46069
GohJunXuan <2400525H@student.tp.edu.sg>
2025-07-16 16:04:23 +0800

    Animations
    Implemented player run and jump animation.
    Need to fix jump animation looping.

--- GADV_Assignment/Assets/Scripts/PlayerController.cs
+++ GADV_Assignment/Assets/Scripts/PlayerController.cs
@@ -1,30 +1,46 @@
+using Unity.VisualScripting;
 using UnityEngine;
-
-public class PlayerMovement : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 8.0f;
+    public float moveSpeed = 9.0f;
     public float jumpSpeed = 7.0f;
     public float fallSpeed = 3.5f;
+    private float animSpeed = 0f;
     [SerializeField] private Vector2 boxSize;
     [SerializeField] private float castDistance;
     [SerializeField] private LayerMask groundLayer;
     private Rigidbody2D rb;
     private float direction = 0f;
+    public Animator animator;
+    bool isFacingLeft = true;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    //Movement
+    //Player Movement
     void Update()
     {
         direction = Input.GetAxisRaw("Horizontal");
         rb.linearVelocity = new Vector2(direction * moveSpeed, rb.linearVelocity.y);
 
+        animSpeed = rb.linearVelocity.x;
+        animator.SetFloat("AnimSpeed", Mathf.Abs(animSpeed));
+
         if (Input.GetButtonDown("Jump") && isGrounded())
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpSpeed);
+            animator.SetBool("IsJumping", true);
+        }
+        //Flip player
+        if (direction > 0 && isFacingLeft)
+        {
+            flipPlayer();
+        }
+        else if (direction < 0 && !isFacingLeft)
+        {
+            flipPlayer();
         }
     }
 
@@ -36,10 +52,10 @@ public class PlayerMovement : MonoBehaviour
             rb.linearVelocity += Vector2.up * Physics.gravity.y * fallSpeed * Time.deltaTime;
         }
     }
-    //Detect if player is on the ground
+    //Check if player is on the ground
     public bool isGrounded()
     {
-        if (Physics2D.BoxCast(transform.position, boxSize, 0, -transform.up, castDistance, groundLayer)) 
+        if (Physics2D.BoxCast(transform.position, boxSize, 0, -transform.up, castDistance, groundLayer))
         {
             return true;
         }
@@ -48,10 +64,17 @@ public class PlayerMovement : MonoBehaviour
             return false;
         }
     }
-
-    //Draw box for ground detection
+    //Show box for grounded detection
     private void OnDrawGizmos()
     {
         Gizmos.DrawWireCube(transform.position - transform.up * castDistance, boxSize);
+    }
+    //Flip player sprite based on direction
+    void flipPlayer()
+    {
+        Vector3 currentScale = gameObject.transform.localScale;
+        currentScale.x *= -1;
+        gameObject.transform.localScale = currentScale;
+        isFacingLeft = !isFacingLeft;
     }
 }
