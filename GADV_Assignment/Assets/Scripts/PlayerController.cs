f5e00fd96f7919516ff9f5566c66d3df7f2acbf1
GohJunXuan <2400525H@student.tp.edu.sg>
2025-07-17 17:22:44 +0800

    Fixed looping jump animation
    Fixed looping jump animation by adding a transition to a new falling animation.
    Cleaned up "PlayerController" by using a box collider component trigger to detect collision instead of code.

--- GADV_Assignment/Assets/Scripts/PlayerController.cs
+++ GADV_Assignment/Assets/Scripts/PlayerController.cs
@@ -5,12 +5,12 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 9.0f;
     public float jumpSpeed = 7.0f;
     public float fallSpeed = 3.5f;
-    private float animSpeed = 0f;
     [SerializeField] private Vector2 boxSize;
     [SerializeField] private float castDistance;
     [SerializeField] private LayerMask groundLayer;
     private Rigidbody2D rb;
     private float direction = 0f;
+    bool isGrounded;
     public Animator animator;
     bool isFacingLeft = true;
 
@@ -24,13 +24,13 @@ public class PlayerController : MonoBehaviour
     {
         direction = Input.GetAxisRaw("Horizontal");
         rb.linearVelocity = new Vector2(direction * moveSpeed, rb.linearVelocity.y);
+        animator.SetFloat("xVelocity", Mathf.Abs(rb.linearVelocity.x));
+        animator.SetFloat("yVelocity", rb.linearVelocity.y);
 
-        animSpeed = rb.linearVelocity.x;
-        animator.SetFloat("AnimSpeed", Mathf.Abs(animSpeed));
-
-        if (Input.GetButtonDown("Jump") && isGrounded())
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpSpeed);
+            isGrounded = false;
             animator.SetBool("IsJumping", true);
         }
         //Flip player
@@ -52,22 +52,11 @@ public class PlayerController : MonoBehaviour
             rb.linearVelocity += Vector2.up * Physics.gravity.y * fallSpeed * Time.deltaTime;
         }
     }
-    //Check if player is on the ground
-    public bool isGrounded()
+    //Check if player is grounded
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (Physics2D.BoxCast(transform.position, boxSize, 0, -transform.up, castDistance, groundLayer))
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-    //Show box for grounded detection
-    private void OnDrawGizmos()
-    {
-        Gizmos.DrawWireCube(transform.position - transform.up * castDistance, boxSize);
+        isGrounded = true;
+        animator.SetBool("IsJumping", false);
     }
     //Flip player sprite based on direction
     void flipPlayer()
