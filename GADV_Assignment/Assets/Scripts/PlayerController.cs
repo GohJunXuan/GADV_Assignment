b0292e406d7a8baac529a495e210e362831af7bf
GohJunXuan <2400525H@student.tp.edu.sg>
2025-07-26 18:42:49 +0800

    Sword Swing and Spikes
    -Implemented sword swing collision
    -Implemented spikes
    -Implemented bouncing off spikes on collision with sword swing hitbox
    -Implemented death animation
    -Implemented death and position reset on player collision with spikes

--- GADV_Assignment/Assets/Scripts/PlayerController.cs
+++ GADV_Assignment/Assets/Scripts/PlayerController.cs
@@ -5,18 +5,20 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 9.0f;
     public float jumpSpeed = 7.0f;
     public float fallSpeed = 3.5f;
-    [SerializeField] private Vector2 boxSize;
-    [SerializeField] private float castDistance;
-    [SerializeField] private LayerMask groundLayer;
+    public float bounceSpeed = 6.0f;
     private Rigidbody2D rb;
     private float direction = 0f;
     bool isGrounded;
+    public GameObject swordHitBox;
+    public float hitBoxRadius;
+    public LayerMask spikes;
     public Animator animator;
     bool isFacingLeft = true;
-
+    Vector2 startPosition;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        startPosition = transform.position;
     }
 
     //Player Movement
@@ -27,12 +29,20 @@ public class PlayerController : MonoBehaviour
         animator.SetFloat("xVelocity", Mathf.Abs(rb.linearVelocity.x));
         animator.SetFloat("yVelocity", rb.linearVelocity.y);
 
+        //Jump
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpSpeed);
             isGrounded = false;
             animator.SetBool("IsJumping", true);
         }
+
+        //Sword Swing
+        if(Input.GetMouseButtonDown(0))
+        {
+            animator.SetBool("IsAttacking", true);
+        }
+
         //Flip player
         if (direction > 0 && isFacingLeft)
         {
@@ -58,6 +68,44 @@ public class PlayerController : MonoBehaviour
         isGrounded = true;
         animator.SetBool("IsJumping", false);
     }
+
+    //Sword Swing
+    public void attack()
+    {
+        Collider2D[] collision = Physics2D.OverlapCircleAll(swordHitBox.transform.position, hitBoxRadius, spikes);
+
+        foreach (Collider2D spikes in collision)
+        {
+            Debug.Log("Collision detected");
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, bounceSpeed);
+        }
+    }
+
+    //End attack animation
+    public void endAttack()
+    {
+        animator.SetBool("IsAttacking", false);
+    }
+
+    //Draw sword hitbox
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(swordHitBox.transform.position, hitBoxRadius);
+    }
+
+    //Play death animation
+    public void death()
+    {
+        animator.SetBool("IsDead", true);
+    }
+
+    //Respawn Player
+    public void respawn()
+    {
+        animator.SetBool("IsDead", false);
+        transform.position = startPosition;
+    }
+
     //Flip player sprite based on direction
     void flipPlayer()
     {
---
+++ GADV_Assignment/Assets/Scripts/Spikes.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Spikes : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.GetComponent<PlayerController>())
+        {
+            collision.collider.GetComponent<PlayerController>().death();
+        }
+    }
+}
