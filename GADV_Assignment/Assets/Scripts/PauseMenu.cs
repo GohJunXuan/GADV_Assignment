57beb5a32ff8a5801be82b247a359ec84bfcf1b6
GohJunXuan <2400525H@student.tp.edu.sg>
2025-08-06 16:25:55 +0800

    Pause Menu
    Implemented Pause Menu

---
+++ GADV_Assignment/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isPaused = false;
+    public GameObject pauseMenuUI;
+
+    void Start()
+    {
+        Resume();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1.0f;
+        isPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0.0f;
+        isPaused = true;
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Exiting Game");
+        Application.Quit();
+    }
+}
